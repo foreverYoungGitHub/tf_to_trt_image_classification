8fc0b07cecab99eb0d0b68faca1c1aec87231509
Thomas Hamböck <th@complang.tuwien.ac.at>
2018-03-22 15:23:44 +0100

    [Sh] Updated test runner.

--- run_tests.sh
+++ run_tests.sh
@@ -8,20 +8,29 @@ cd ..
 TIME="/usr/bin/time --append -o data/timings.txt "
 
 echo "32bit float"
-echo "32bit float conversion" >> data/timings.txt 
-$TIME python scripts/frozen_graphs_to_plans.py float
+if [ ! -f data/plans_f32.done ]; then
+    echo "32bit float conversion" >> data/timings.txt 
+    $TIME python scripts/frozen_graphs_to_plans.py float
+    touch data/plans_f32.done
+fi
 echo "32bit float inference" >> data/timings.txt 
 $TIME python scripts/test_trt.py float
 
 echo "16bit float"
-echo "16bit float conversion" >> data/timings.txt 
-$TIME python scripts/frozen_graphs_to_plans.py half
+if [ ! -f data/plans_f16.done ]; then
+    echo "16bit float conversion" >> data/timings.txt 
+    $TIME python scripts/frozen_graphs_to_plans.py half
+    touch data/plans_f16.done
+fi
 echo "16bit float inference" >> data/timings.txt 
 $TIME python scripts/test_trt.py half
 
 echo "8bit integer"
-echo "8bit integer conversion" >> data/timings.txt
-$TIME python scripts/frozen_graphs_to_plans.py int8
+if [ ! -f data/plans_i8.done ]; then
+    echo "8bit integer conversion" >> data/timings.txt
+    $TIME python scripts/frozen_graphs_to_plans.py int8
+    touch data/plans_i8.done
+fi
 echo "8bit integer inference" >> data/timings.txt
 $TIME python scripts/test_trt.py int8
 
